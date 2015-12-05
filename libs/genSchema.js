d18599084dba75f94dbc038c1433c08b6cbf81a0
Onurbon <marnette.bruno@gmail.com>
2015-12-05 16:47:46 +0000

    fix schema generation

--- libs/genSchema.js
+++ libs/genSchema.js
@@ -5,27 +5,51 @@ var GraphQLString = require('graphql').GraphQLString;
 
 let count = 0;
 
-function schemaFromData() {
-  return new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: 'email',
-      fields: {
-        id: {
-          type: GraphQLInt,
-          resolve: (row) => row.id,
-        },
-        name: {
-          type: GraphQLString,
-          resolve: (row) => row.name,
-        },
-        email: {
-          type: GraphQLString,
-          resolve: (row) => row.email,
-        }
-      }
-    })
-  });
+
+// http://stackoverflow.com/questions/10834796/validate-that-a-string-is-a-positive-integer
+function isNormalInteger(str) {
+    var n = ~~Number(str);
+    return String(n) === str && n >= 0;
 }
 
+function sanitize(name) {
+  return name.replace(/\./g, '_').replace(/\//g, '_');
+}
 
-module.exports.schemaFromData = schemaFromData;
+function schemaFromArrayOfObjects(name, data) {
+  var firstRow = data[0];
+  var fieldsFromData = {};
+  // inferring types (Int or String) from first row
+  Object.keys(firstRow).forEach(fieldName => {
+    var val = firstRow[fieldName];
+    fieldsFromData[fieldName] = {
+      type: isNormalInteger(val) ? GraphQLInt : GraphQLString,
+      description: 'Example value: ' + val,
+      resolve: (row) => row[fieldName]
+    }
+  });
+  return new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: sanitize(name),
+      fields: () => fieldsFromData,
+    })
+  })
+}
+
+function schemaFromSpreadSheet(name, obj) {
+  var fieldsFromData = {};
+  Object.keys(obj).forEach(sheetName => {
+    fieldsFromData[sheetName] = {
+      type: schemaFromArrayOfObjects(sheetName, obj[sheetName]),
+      description: sheetName + ' sheet',
+      resolve: (root, {sname}) => obj[sname],
+    }
+  });
+  return new GraphQLObjectType({
+    name: sanitize(name),
+    description: 'File ' + name,
+    fields: () => fieldsFromData,
+  })
+}
+
+module.exports.schemaFromSpreadSheet = schemaFromSpreadSheet;
