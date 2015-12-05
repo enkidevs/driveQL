edb83a1c9dcf027d29952b7f129fb8d5b84bab7d
Onurbon <marnette.bruno@gmail.com>
2015-12-05 16:15:50 +0000

    fixture schema...

--- libs/genSchema.js
+++ libs/genSchema.js
@@ -1,21 +1,31 @@
-var GraphQLObjectType = require('graphql/lib/type').GraphQLObjectType;
-var GraphQLSchema = require('graphql/lib/type').GraphQLSchema;
-var GraphQLInt = require('graphql/lib/type').GraphQLInt;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLSchema = require('graphql').GraphQLSchema;
+var GraphQLInt = require('graphql').GraphQLInt;
+var GraphQLString = require('graphql').GraphQLString;
 
 let count = 0;
 
-let schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      count: {
-        type: GraphQLInt,
-        resolve: function() {
-          return count;
+function schemaFromData() {
+  return new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'email',
+      fields: {
+        id: {
+          type: GraphQLInt,
+          resolve: (row) => row.id,
+        }
+        name: {
+          type: GraphQLString,
+          resolve: (row) => row.name,
+        }
+        email: {
+          type: GraphQLString,
+          resolve: (row) => row.email,
         }
       }
-    }
-  })
-});
+    })
+  });
+}
 
-module.exports.schema = schema;
+
+module.exports.schemaFromData = schemaFromData;
